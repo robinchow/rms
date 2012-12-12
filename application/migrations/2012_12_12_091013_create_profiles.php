6b446eb939e6f2e05ed0328bacd1294cce2b88f2
Christopher Manouvrier <cmanou@mac.com>
2012-12-12 21:44:55 +1100

    User profile migration

---
+++ application/migrations/2012_12_12_091013_create_profiles.php
@@ -0,0 +1,42 @@
+<?php
+
+class Create_Profiles {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('profiles', function($table) {
+		    $table->increments('id');
+    		$table->string('full_name', 128);
+    		$table->string('display_name', 128);
+    		$table->string('gender',1);
+    		$table->date('dob');
+    		$table->string('image')->unique();
+    		$table->boolean('privacy');
+    		$table->string('phone',12);
+    		$table->string('university');
+    		$table->string('program');
+    		$table->string('student_number')->nullable();
+    		$table->date('start_year');
+    		$table->boolean('arc');
+    		$table->integer('user_id');
+		    $table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('profiles');
+
+	}
+
+}
