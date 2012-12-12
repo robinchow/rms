2f2bb0a54d10c02b2a297452e2a0eb11a8706d09
Christopher Manouvrier <cmanou@mac.com>
2012-12-12 22:13:49 +1100

    User year migration

---
+++ application/migrations/2012_12_12_111048_create_user_year.php
@@ -0,0 +1,31 @@
+<?php
+
+class Create_User_Year {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user_year', function($table) {
+		    $table->increments('id');
+		    $table->integer('user_id');
+		    $table->integer('year_id');
+		    $table->timestamps();
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_year');
+
+	}
+
+}
