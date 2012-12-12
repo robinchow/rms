95c8b35e22c5bf685a1db7b47b1ade1cf65aeafc
Christopher Manouvrier <cmanou@mac.com>
2012-12-12 22:59:01 +1100

    Added bullshit seed

---
+++ application/migrations/2012_12_12_115711_create_seeds.php
@@ -0,0 +1,28 @@
+<?php
+
+class Create_Seeds {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::table('users')->insert(array(
+		    'email'  => 'example@example.com',
+		    'password'  => Hash::make('example')
+		));
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
