7b0f5b7bc8e15364e2adfa7cb13ada0ad71d6d41
Christopher Manouvrier <cmanou@mac.com>
2013-01-05 00:04:29 +1100

    Magic

--- application/models/year.php
+++ application/models/year.php
@@ -8,6 +8,7 @@ class Year extends Eloquent {
         return $this->has_many_and_belongs_to('User');
     }
 
+    //Dont know if this is dodgy
     public function executives()
     {
         return $this->has_many_and_belongs_to('Executive','executive_user')->with('user_id','non_executive');
