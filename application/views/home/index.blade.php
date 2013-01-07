f8673435f89a94ef42507c600c1f736e2923029a
Christopher Manouvrier <cmanou@mac.com>
2013-01-07 20:45:17 +1100

    Fixed some responsive problems with front end

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -6,17 +6,10 @@
 
 @section('content')
 	<div class="row">
-	    <div class="span8 offset1" id="main-title">
-			<h2>What is CSE Revue?&nbsp;</h2> 
-		</div>
-
-		<div class="span2" id="main-title">
-			<h2>Sponsors&nbsp;</h2> 
-		</div>
-	</div>
-
-	<div class="row">
 		<div class="span8 offset1">
+			<div id="main-title">
+			<h2>What is CSE Revue?&nbsp;</h2> 
+			</div>
 			<div id="main-content">
 				<p>The Computer Science and Engineering (CSE) Revue is a live comedy sketch show held during September at the University of New South Wales (UNSW). Produced and directed by members of the society, the show serves to highlight the technical and creative talents of UNSW students, as well as an opportunity for students to further develop their university experience.</p>
 				<p>Read more about us here.</p>
@@ -42,15 +35,18 @@
 			</div>
 		</div>
 
+		<div class="span2">
+			<div id="main-title">
+				<h2>Sponsors&nbsp;</h2> 
+			</div>
 
-		<div class="span2" id="main-content">
-			<center>
-			@foreach($sponsors as $sponsor)
-				<p><a href="{{ $sponsor->url}}">
-				<img src="/img/sponsor/{{ $sponsor->image }}" width="100px" height="100px"/>
-				</a><p>
-			@endforeach
-		</center>
+			<div id="sponsors">
+				@foreach($sponsors as $sponsor)
+					<a href="{{ $sponsor->url}}">
+					<img src="/img/sponsor/{{ $sponsor->image }}"/>
+					</a>
+				@endforeach
+			</div>
 		</div>
 	</div>
       
