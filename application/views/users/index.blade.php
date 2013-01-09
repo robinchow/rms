8814b4649e62006fa1dc2ed85df8061ea5824962
Christopher Manouvrier <cmanou@mac.com>
2013-01-09 18:04:17 +1100

    Fixed users view

--- application/views/users/index.blade.php
+++ application/views/users/index.blade.php
@@ -18,10 +18,29 @@
 		<tr>
 			<td>{{$user->profile->full_name}}</td>
 			<td>{{$user->profile->display_name}}</td>
-			<td>{{$user->admin}}</td>
-			<td>{{ HTML::link('rms/users/make_admin/'. $user->id,'Make Admin')}} - 
-				{{ HTML::link('rms/users/remove_admin/'. $user->id,'Remove Admin')}} - 
-				{{ HTML::link('rms/users/show/'. $user->id,'Show')}}</td>
+			<td>
+				@if($user->admin)
+					<i class="icon-ok"></i>
+				@else
+					<i class="icon-remove"></i>
+				@endif
+			</td>
+			<td>
+				<div class="btn-group">
+					<a class="btn btn-primary" href="/rms/users/show/{{$user->id}}"><i class="icon-user icon-white"></i> Profile</a>
+					<a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#"><i class="icon-pencil"></i> Edit</a></li>
+						<li><a href="#"><i class="icon-trash"></i> Delete</a></li>
+						<li class="divider"></li>
+						@if($user->admin)
+							<li><a href="/rms/users/remove_admin/{{$user->id}}"><i class="i"></i> Remove admin</a></li>
+						@else
+							<li><a href="/rms/users/make_admin/{{$user->id}}"><i class="i"></i> Make admin</a></li>
+						@endif
+					</ul>
+				</div>
+			</td>
 		</tr>
 	@endforeach
 	</table>
