c4fab9289a3c7d5c38af95c97eb8912b22be6857
Christopher Manouvrier <cmanou@mac.com>
2013-01-09 18:04:31 +1100

    Removed edit

--- application/views/users/index.blade.php
+++ application/views/users/index.blade.php
@@ -30,9 +30,6 @@
 					<a class="btn btn-primary" href="/rms/users/show/{{$user->id}}"><i class="icon-user icon-white"></i> Profile</a>
 					<a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
 					<ul class="dropdown-menu">
-						<li><a href="#"><i class="icon-pencil"></i> Edit</a></li>
-						<li><a href="#"><i class="icon-trash"></i> Delete</a></li>
-						<li class="divider"></li>
 						@if($user->admin)
 							<li><a href="/rms/users/remove_admin/{{$user->id}}"><i class="i"></i> Remove admin</a></li>
 						@else
