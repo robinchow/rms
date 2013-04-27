6543c6082ff2a6bc8f22646d331fbb6d4635606e
Christopher Manouvrier <cmanou@mac.com>
2013-04-27 15:57:44 +1000

    Fixing up signup form

--- application/views/camp/registrations/signup.blade.php
+++ application/views/camp/registrations/signup.blade.php
@@ -5,27 +5,45 @@
 @endsection
 
 @section('content')
-    {{ Form::open('rms/camp/settings/add')}}
+    {{ Form::open('rms/camp/registrations/signup')}}
 
     <legend>Add New Camp</legend>
         {{ Form::label('camp_setting_id', 'Camp') }}
         {{ Form::select('camp_setting_id', $camps) }}<br>
 
-        {{ Form::label('theme', 'Theme') }}
-        {{ Form::text('theme',Input::old('theme'))}}<br>
+        <!--user Details-->
+        {{ Form::hidden('user_id',Auth::User()->id)}}<br>
+
+
+
+        <label for="medical" class="checkbox">
+            {{ Form::checkbox('medical', 1,Input::old('medical') ) }} Medical Conditions
+        </label>
+
+        {{ Form::label('medical_conditions', 'Medical Conditions') }}
+        {{ Form::textarea('medical_conditions',Input::old('medical_conditions'))}}<br>
+
+        <label for="dietary" class="checkbox">
+            {{ Form::checkbox('dietary', 1,Input::old('dietary') ) }} Dietary Requirments
+        </label>
+
+        {{ Form::label('dietary_requirments', 'Dietary Requirments') }}
+        {{ Form::textarea('dietary_requirments',Input::old('dietary_requirments'))}}<br>
+
 
         {{ Form::label('places', 'Places') }}
         {{ Form::text('places',Input::old('places'))}}<br>
 
-		{{ Form::label('details', 'Details') }}
-        {{ Form::textarea('details',Input::old('details'))}}<br>
+        {{ Form::label('places', 'Places') }}
+        {{ Form::text('places',Input::old('places'))}}<br>
 
-        <label for="privacy" class="checkbox">
-            {{ Form::checkbox('visible', 1,Input::old('visible') ) }} Visible
-        </label>
+		{{ Form::label('song_requests', 'Song Requests') }}
+        {{ Form::textarea('song_requests',Input::old('song_requests'))}}<br>
+
+
 
         {{ Form::submit('Save changes',array('class'=>'btn btn-primary')) }}
-        {{ HTML::link('/rms/camp/settings','Cancel',array('class'=>'btn')) }}
+        {{ HTML::link('/rms/camp/registrations','Cancel',array('class'=>'btn')) }}
 
     {{ Form::close() }}
       
