e916412293e05cb8a32fc3492ddb8670d78a31e4
Timothy Wiley <timothyw@cse.unsw.edu.au>
2013-04-27 17:01:30 +1000

    Testing for using subviews before making team joining ajax

--- application/views/teams/index.blade.php
+++ application/views/teams/index.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+
 <h2>Teams</h2>
 @if ( count($teams) > 0 )
     <table class="table table-bordered table-striped">
@@ -21,24 +22,7 @@
 
     	<td>{{$team->privacy_string}}</td>
         <td>
-            <div class="btn-group">
-                    @if(Auth::user()->is_part_of_team(Year::current_year()->id,$team->id) && !$team->privacy && $team->is_active())
-                        <a class="btn" href="/rms/teams/member_leave/{{$team->id}}">Leave</a>
-                    @elseif(!$team->privacy && $team->is_active())
-                        <a class="btn" href="/rms/teams/member_join/{{$team->id}}">&nbsp;Join&nbsp;&nbsp;</a>
-                    @endif
-
-                <a class="btn btn-primary" href="/rms/teams/show/{{$team->id}}">View</a>
-                @if(Auth::User()->admin or Auth::User()->can_manage_team(Year::current_year()->id, $team->id))
-                <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li>{{HTML::link('rms/teams/manage/'. $team->id,'Manage Team')}}</li>
-                    <li>{{HTML::link('rms/teams/edit/'. $team->id,'Edit Team')}}</li>
-                    <li>{{HTML::link('rms/teams/delete/'. $team->id,'Delete Team')}}</li>
-
-                </ul>
-                @endif
-            </div>
+            @include('teams.index.joinleave')
         </td>
     	<tr>
 	@endforeach
