9cda9fc5e9589634c130019d6dc1174393054b9c
Christopher Manouvrier <cmanou@mac.com>
2013-08-09 23:32:51 +1000

    Fixed totals in merch order

--- application/models/merch/order.php
+++ application/models/merch/order.php
@@ -17,4 +17,16 @@ class Merch_Order extends Eloquent {
     {
         return $this->has_many_and_belongs_to('Merch_Item', 'merch_item_order')->with('quantity','size');
     }
+
+    public function total() {
+    	$total = 0.0;
+    	foreach ($this->items as $i) {
+    		$total += $i->price * $i->pivot->quantity;
+    	}
+    	return $total;
+    }
+
+    public function remaining() {
+    	return ($this->total() - $this->amount_paid);
+    }
 }
