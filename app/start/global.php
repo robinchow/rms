26046ca63dffc250edeebd47277505f9d193644f
Stephen Sherratt <=>
2014-02-17 17:17:56 +1100

    added new 404 error handler

--- app/start/global.php
+++ app/start/global.php
@@ -51,6 +51,13 @@ App::error(function(Exception $exception, $code)
 	Log::error($exception);
 });
 
+
+App::missing(function($exception)
+{
+    return Response::view('error.404', array(), 404);
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Maintenance Mode Handler
