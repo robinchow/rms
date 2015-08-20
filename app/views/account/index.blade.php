48700edd2c18f26b39bda9c1ded2682893269481
vincentt143 <tran.vincent17@gmail.com>
2015-08-20 10:37:55 +1000

    Fix XSS vulnerability

--- app/views/account/index.blade.php
+++ app/views/account/index.blade.php
@@ -20,7 +20,7 @@
 @section('content')
     {{ HTML::image($user->image_url, $user->profile->full_name, array('width'=>'200px','height'=>'200px','class'=>'pull-right')) }}
     
-    <h2>{{$user->profile->full_name}}</h2>
+    <h2>{{{$user->profile->full_name}}}</h2>
 
     @if ($user->receive_emails)
         <p>You are subscribed to our general mailing list!</p>
@@ -32,23 +32,23 @@
 
     <h3>Personal Details:</h3>
 
-    <p><strong>Full Name: </strong>{{ $user->profile->full_name }}</p>
+    <p><strong>Full Name: </strong>{{{ $user->profile->full_name }}}</p>
     @if($user->profile->display_name != "")
-        <p><strong>Display Name: </strong>{{ $user->profile->display_name }}</p>
+        <p><strong>Display Name: </strong>{{{ $user->profile->display_name }}}</p>
     @endif
-    <p><strong>DOB: </strong>{{  $user->profile->dob }}</p>
-    <p><strong>Gender: </strong>{{ $user->profile->gender }}</p>
+    <p><strong>DOB: </strong>{{{  $user->profile->dob }}}</p>
+    <p><strong>Gender: </strong>{{{ $user->profile->gender }}}</p>
 
     <h3>Contact Details:</h3>
-    <p><strong>Email: </strong>{{ $user->email }}</p>
-    <p><strong>Phone: </strong>{{ $user->profile->phone }}</p>
+    <p><strong>Email: </strong>{{{ $user->email }}}</p>
+    <p><strong>Phone: </strong>{{{ $user->profile->phone }}}</p>
 
     <h3>University Details:</h3>
-    <p><strong>University: </strong>{{ $user->profile->university }}</p>
-    <p><strong>Program: </strong>{{ $user->profile->program }}</p>
-    <p><strong>Student Number: </strong>{{ $user->profile->student_number }}</p>
-    <p><strong>Start Year: </strong>{{ $user->profile->start_year }}</p>
-    <p><strong>Arc: </strong>{{ $user->profile->arc_string }}</p>
+    <p><strong>University: </strong>{{{ $user->profile->university }}}</p>
+    <p><strong>Program: </strong>{{{ $user->profile->program }}}</p>
+    <p><strong>Student Number: </strong>{{{ $user->profile->student_number }}}</p>
+    <p><strong>Start Year: </strong>{{{ $user->profile->start_year }}}</p>
+    <p><strong>Arc: </strong>{{{ $user->profile->arc_string }}}</p>
 
     <h3>My Revue:</h3>
     @foreach($user->years as $year)
