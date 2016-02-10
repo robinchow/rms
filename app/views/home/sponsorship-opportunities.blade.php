9e117e055bf96467927587335ac30e9d55b4965c
vincentt143 <tran.vincent17@gmail.com>
2016-02-10 15:43:33 +1100

    Update sponsorship proposal

--- app/views/home/sponsorship-opportunities.blade.php
+++ app/views/home/sponsorship-opportunities.blade.php
@@ -16,7 +16,7 @@
         <div class="span10 offset1" id="main-content">
             <div class="card">
                 <p><a class="clear" href="/media/CSERevueSponsorshipProposal.pdf"><img src="/img/CSERevueSponsorshipProposal.png" style="float: left; margin-right: 15px;" width="153px;" height="197px"></a></p>
-                <p>For information about how you can sponsor CSE Revue, please see our&nbsp;<br><a href="/media/CSERevueSponsorshipProposal.pdf"><i class="icon-file"></i>&nbsp;2015 Sponsorship Proposal</a>.</p>
+                <p>For information about how you can sponsor CSE Revue, please see our&nbsp;<br><a href="/media/CSERevueSponsorshipProposal.pdf"><i class="icon-file"></i>&nbsp;2016 Sponsorship Proposal</a>.</p>
                 <p>For further enquiries and to discuss sponsorship opportunities, please email&nbsp;<br><a href="mailto:producers@cserevue.org.au"><i class="icon-envelope"></i>&nbsp;producers@cserevue.org.au</a></p>
             </div>
         </div>
